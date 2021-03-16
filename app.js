ffc34faf7e1938ec8f26689c993c2c4c14eda86c
JorgeJoseAbad <jorgejoseabad@gmail.com>
2021-03-16 20:27:56 +0100

    change var for const en appjs

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ const createMongoStorage = require("connect-mongo")
 const MongoStore = createMongoStorage(session);
 
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var user = require('./routes/users');
+const index = require('./routes/index');
+const user = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
